bb7db483d2ce85bb2a4358ee3fdd5bc92ac562b0
Wade Mealing <wmealing@redhat.com>
2024-01-02 17:31:13 +1000

    Debugging for openrgb testing #1.

--- quantum/rgb_matrix/animations/openrgb_direct_anim.h
+++ quantum/rgb_matrix/animations/openrgb_direct_anim.h
@@ -3,6 +3,7 @@ RGB_MATRIX_EFFECT(OPENRGB_DIRECT)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 #include "openrgb.h"
+#include "print.h"
 
 bool OPENRGB_DIRECT(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
@@ -10,6 +11,13 @@ bool OPENRGB_DIRECT(effect_params_t* params) {
     for (uint8_t i = led_min; i < led_max; i++) {
         #ifdef OPENRGB_DIRECT_MODE_USE_UNIVERSAL_BRIGHTNESS
         float brightness = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+
+        xprintf("ORGBDIRECT univeral: i(%d) RGB(%d %d %d)\n" i,
+                brightness * g_openrgb_direct_mode_colors[i].r,
+                brightness * g_openrgb_direct_mode_colors[i].g,
+                brightness * g_openrgb_direct_mode_colors[i].b,
+                )
+
         rgb_matrix_set_color(
             i,
             brightness * g_openrgb_direct_mode_colors[i].r,
@@ -17,6 +25,13 @@ bool OPENRGB_DIRECT(effect_params_t* params) {
             brightness * g_openrgb_direct_mode_colors[i].b
         );
         #else
+
+        xprintf("ORGBDIRECT non-univeral: i(%d) RGB(%d %d %d)\n" i,
+                brightness * g_openrgb_direct_mode_colors[i].r,
+                brightness * g_openrgb_direct_mode_colors[i].g,
+                brightness * g_openrgb_direct_mode_colors[i].b,
+                )
+
         rgb_matrix_set_color(
             i,
             g_openrgb_direct_mode_colors[i].r,
